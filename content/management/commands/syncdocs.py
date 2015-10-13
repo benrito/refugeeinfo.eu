97b1770f541dd2b59e35aa68b62ab98193f23883
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-10-13 18:07:34 +0300

    Management

---
+++ content/management/commands/syncdocs.py
@@ -0,0 +1,73 @@
+from __future__ import absolute_import, unicode_literals, division, print_function
+
+__author__ = 'reyrodrigues'
+
+import os
+
+import httplib2
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+
+from django.core.management.base import BaseCommand
+from content import models
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('credentials', type=unicode)
+
+    def handle(self, *args, **options):
+        credentials_text = options['credentials']
+        credentials = oauth2client.client.Credentials.new_from_json(credentials_text)
+
+        models.LocationContent.objects.all()
+        documents, includes = self._list_folders(credentials)
+        for key in documents.iterkeys():
+            if '-' in key:
+                identifier = key.split('-')
+                existing_content = models.LocationContent.objects.filter(parent__slug=identifier[0],
+                                                                         language__iso_code=identifier[1])
+                if existing_content:
+                    content = existing_content[0]
+                    content.google_doc = documents[key]
+                    content.save()
+                else:
+                    location = models.Location.objects.get(slug=identifier[0])
+                    language = models.Language.objects.get(iso_code=identifier[1])
+
+                    models.LocationContent.objects.create(parent=location, language=language, google_doc=documents[key])
+
+        self.stdout.write('Successfully created/updated content!')
+
+    def _list_folders(self, credentials):
+        http = credentials.authorize(httplib2.Http())
+        service = discovery.build('drive', 'v2', http=http)
+
+        folder_id = os.environ.get('FOLDER_ID', '')
+
+        request = service.children().list(folderId=folder_id,
+                                          q="mimeType= 'application/vnd.google-apps.document'"
+                                            " or mimeType= 'application/vnd.google-apps.folder'")
+
+        main_folder = request.execute().get('items', [])
+        expanded = [e for e in self._expand_folder(service, main_folder) if 'exportLinks' in e]
+
+        documents = dict([(p['title'], p['exportLinks']['text/html']) for p in expanded if 'include' not in p['title']])
+        includes = dict([(p['title'], p['exportLinks']['text/html']) for p in expanded if 'include' in p['title']])
+
+        return documents, includes
+
+    def _expand_folder(self, service, parent):
+        children = []
+        for p in parent:
+            f = service.files().get(fileId=p['id']).execute()
+            if f['mimeType'] == 'application/vnd.google-apps.folder':
+                grand_children = service.children().list(folderId=f['id']).execute().get('items', [])
+                children += self._expand_folder(service, grand_children)
+
+            children.append(f)
+
+        return children
