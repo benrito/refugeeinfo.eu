68a0a1e0f117a78290ea5bbb7edb2f4265c0df6c
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-10-29 15:57:28 +0200

    CMS Auth and CSS

--- content/utils.py
+++ content/utils.py
@@ -8,6 +8,7 @@ from lxml import etree
 import requests
 from StringIO import StringIO
 
+
 def get_cms_url(language, slug):
     return settings.CMS_URL + '/{}/{}/{}/'.format(language, settings.CMS_ENVIRONMENT, slug)
 
@@ -16,12 +17,18 @@ def get_cms_page(language, slug):
     cms_url = get_cms_url(language, slug)
     cms_url_en = get_cms_url(language, slug)
 
-    r = requests.get(cms_url, headers={"Accept-Language": language})
+    r = requests.get(cms_url,
+                     headers={"Accept-Language": language},
+                     auth=(settings.CMS_USER, settings.CMS_PASSWORD)
+    )
 
     if r.status_code == 200:
         html_content = r.text
     elif language != 'en':
-        r = requests.get(cms_url_en, headers={"Accept-Language": 'en'})
+        r = requests.get(cms_url_en,
+                         headers={"Accept-Language": language},
+                         auth=(settings.CMS_USER, settings.CMS_PASSWORD)
+        )
         if r.status_code == 200:
             html_content = r.text
         else:
