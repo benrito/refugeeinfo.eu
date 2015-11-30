2f7828dacc229b6547170c381a2ae68a038ea80e
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2015-11-30 17:49:39 -0500

    Clock

--- clock.py
+++ clock.py
@@ -14,18 +14,10 @@ import sys
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=15)
 def one_minute():
     subprocess.call(['python', 'manage.py', 'syncdocs'], stderr=sys.stderr, stdout=sys.stdout)
 
     print('This job is run every three minutes.')
-
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('This job is run every three minutes.')
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-def scheduled_job():
-    print('This job is run every weekday at 5pm.')
 
 sched.start()
